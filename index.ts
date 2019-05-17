eb1b84aafee86aa46a282fdee0bd3deadf51b904
Anand Chowdhary <mail@anandchowdhary.com>
2019-05-17 17:56:59 +0200

    :zap: Use most recent version for Unpkg

--- index.ts
+++ index.ts
@@ -7,6 +7,6 @@ export default (query: string, fallback: string) => {
   if (!query) return fallback;
   query = slugify(query);
   if (browserIcons.includes(query)) return `https://cdnjs.cloudflare.com/ajax/libs/browser-logos/51.0.13/${query}/${query}_128x128.png`;
-  if (hostedIcons.includes(query)) return `https://unpkg.com/analytics-icons@1.0.0/icons/${query}.png`;
+  if (hostedIcons.includes(query)) return `https://unpkg.com/analytics-icons/icons/${query}.png`;
   return fallback;
 }
